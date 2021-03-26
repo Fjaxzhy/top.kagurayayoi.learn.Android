b6b1da720572c3b7f95044451dc8893e1f2e1410
kagurayayoi <862883646@qq.com>
2021-03-26 15:43:48 +0800

    显式/隐式Intent

---
+++ Intent/app/src/main/java/top/kagurayayoi/learn/android/intent/SecondActivity.java
@@ -0,0 +1,25 @@
+package top.kagurayayoi.learn.android.intent;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SecondActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+        Button button = findViewById(R.id.button2);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent("top.kagurayayoi.learn.android.intent.ACTION_START");
+                startActivity(intent);
+            }
+        });
+    }
+}
