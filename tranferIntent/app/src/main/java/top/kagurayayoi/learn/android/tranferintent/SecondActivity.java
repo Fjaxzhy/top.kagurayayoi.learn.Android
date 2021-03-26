4dc32f08aed25770f96c63fdaa8a3fd5076ed92f
kagurayayoi <862883646@qq.com>
2021-03-26 19:25:40 +0800

    Intent传递参数

---
+++ tranferIntent/app/src/main/java/top/kagurayayoi/learn/android/tranferintent/SecondActivity.java
@@ -0,0 +1,19 @@
+package top.kagurayayoi.learn.android.tranferintent;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SecondActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+        Intent intent = getIntent();
+        String data = intent.getStringExtra("extra_data");
+        Toast.makeText(SecondActivity.this,data,Toast.LENGTH_SHORT).show();
+    }
+}
