a49f5cdf7403f7e70832325085bf9e884ebfc160
kagurayayoi <862883646@qq.com>
2021-03-26 12:58:40 +0800

    使用Toast

---
+++ useToast/app/src/main/java/top/kagurayayoi/learn/android/usetoast/MainActivity.java
@@ -0,0 +1,24 @@
+package top.kagurayayoi.learn.android.usetoast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this,"You clicked Button",Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
