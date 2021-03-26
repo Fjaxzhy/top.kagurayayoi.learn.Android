b8bc1325bfdc6c813c60dbfd6ced30fa08535f63
kagurayayoi <862883646@qq.com>
2021-03-26 13:16:14 +0800

    使用Menu

---
+++ useMenu/app/src/main/java/top/kagurayayoi/learn/android/usemenu/MainActivity.java
@@ -0,0 +1,38 @@
+package top.kagurayayoi.learn.android.usemenu;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.add_item:
+                Toast.makeText(this,"You clicked Add",Toast.LENGTH_SHORT);
+                break;
+            case R.id.remove_item:
+                Toast.makeText(this,"You clicked Remove",Toast.LENGTH_SHORT);
+                break;
+            default:
+        }
+        return true;
+    }
+}
