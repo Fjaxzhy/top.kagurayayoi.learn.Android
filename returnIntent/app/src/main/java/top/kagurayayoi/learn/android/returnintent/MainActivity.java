275ce013e086dede380d0ad651d37d4e4192d116
kagurayayoi <862883646@qq.com>
2021-03-27 14:44:29 +0800

    返回数据给上一个活动

---
+++ returnIntent/app/src/main/java/top/kagurayayoi/learn/android/returnintent/MainActivity.java
@@ -0,0 +1,40 @@
+package top.kagurayayoi.learn.android.returnintent;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,SecondActivity.class);
+                startActivityForResult(intent,1);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        switch (requestCode){
+            case 1:
+                if (resultCode == RESULT_OK){
+                    String returnData = data.getStringExtra("data_return");
+                    Toast.makeText(MainActivity.this,returnData,Toast.LENGTH_SHORT).show();
+                }
+                break;
+            default:
+        }
+    }
+}
---
+++ returnIntent/app/src/main/java/top/kagurayayoi/learn/android/returnintent/SecondActivity.java
@@ -0,0 +1,27 @@
+package top.kagurayayoi.learn.android.returnintent;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SecondActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+        Button button2 = findViewById(R.id.button2);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.putExtra("data_return","This is data_return");
+                setResult(RESULT_OK,intent);
+                finish();
+            }
+        });
+    }
+}
