f34b29a5bab9125471657ccc693084f3bd10ea48
kagurayayoi <862883646@qq.com>
2021-03-28 12:58:33 +0800

    活动的启动模式 standard

---
+++ standard/app/src/main/java/top/kagurayayoi/learn/android/standard/MainActivity.java
@@ -0,0 +1,28 @@
+package top.kagurayayoi.learn.android.standard;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Log.e("MainActivity",this.toString());
+        // E/MainActivity: top.kagurayayoi.learn.android.standard.MainActivity
+        setContentView(R.layout.activity_main);
+        Button button = findViewById(R.id.button1);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,MainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
